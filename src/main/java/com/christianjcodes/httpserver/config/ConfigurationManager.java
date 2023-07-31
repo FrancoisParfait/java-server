981d6d9b9028becbf36e2f8fa4ea3de03f19f5ad
Christian Johnson <cartian4@gmail.com>
2023-07-31 06:15:51 -0400

    Add instance of ConfigurationManager class, add loadConfiguration and getCurrentConfiguration methods

--- src/main/java/com/christianjcodes/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/christianjcodes/httpserver/config/ConfigurationManager.java
@@ -1,4 +1,30 @@
 package com.christianjcodes.httpserver.config;
 
 public class ConfigurationManager {
+
+    private static ConfigurationManager myConfigurationManager;
+
+    private ConfigurationManager() {
+    }
+
+    public static ConfigurationManager getInstance() {
+        if (myConfigurationManager == null) {
+            myConfigurationManager = new ConfigurationManager();
+        }
+        return myConfigurationManager;
+    }
+
+    /**
+     * Used to load a configuration file by the provided path
+     */
+    public void loadConfigurationFile(String filePath) {
+
+    }
+
+    /**
+     * Returns the current loaded Configuration
+     */
+    public void getCurrentConfiguration() {
+
+    }
 }
