80c49d48d26c2c9830d8493c5c217c5f3858eb81
Christian Johnson <cartian4@gmail.com>
2023-07-31 05:59:58 -0400

    Add variables for port and webroot, add getter and setter methods for port and webroot

--- src/main/java/com/christianjcodes/httpserver/config/Configuration.java
+++ src/main/java/com/christianjcodes/httpserver/config/Configuration.java
@@ -1,4 +1,23 @@
 package com.christianjcodes.httpserver.config;
 
 public class Configuration {
+
+    private int port;
+    private String webroot;
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getWebroot() {
+        return webroot;
+    }
+
+    public void setWebroot(String webroot) {
+        this.webroot = webroot;
+    }
 }
