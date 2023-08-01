b57c6241b74b8e9af330399113bb398a0b5d4eb8
Christian Johnson <cartian4@gmail.com>
2023-08-01 06:00:17 -0400

    Add defaultObjectMapper method

--- src/main/java/com/christianjcodes/httpserver/util/Json.java
+++ src/main/java/com/christianjcodes/httpserver/util/Json.java
@@ -1,9 +1,16 @@
 package com.christianjcodes.httpserver.util;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Json {
 
-    private static ObjectMapper myObjectMapper;
+    private static ObjectMapper myObjectMapper = defaultObjectMapper();
+
+    public static ObjectMapper defaultObjectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return om;
+    }
 
 }
