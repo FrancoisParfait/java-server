b7f730f9351fd8c103c5c305187106ae1f777ff8
Christian Johnson <cartian4@gmail.com>
2023-08-03 06:06:17 -0400

    Move socket code from HttpServer to ServerListenerThread

--- src/main/java/com/christianjcodes/httpserver/HttpServer.java
+++ src/main/java/com/christianjcodes/httpserver/HttpServer.java
@@ -27,34 +27,5 @@ public class HttpServer {
         System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using WebRoot: " + conf.getWebroot());
 
-        try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>Simple Java HTTP Server</title></head" +
-                    "<body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
-
-            final String CRLF = "\n\r"; // 13, 10
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "Content-Length: " + html.getBytes().length + CRLF + // HEADER
-                        CRLF +
-                        html +
-                        CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
