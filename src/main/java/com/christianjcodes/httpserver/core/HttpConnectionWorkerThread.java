e8a6dbbbf0cae867a4c609be6ea9c8f56c758ccf
Christian Johnson <cartian4@gmail.com>
2023-08-14 06:25:03 -0400

    Rename HttpConnectionWorker to HttpConnectionWorkerThread, move content from run method of ServerListenerThread to HttpConnectionWorkerThread

---
+++ src/main/java/com/christianjcodes/httpserver/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,60 @@
+package com.christianjcodes.httpserver.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HttpConnectionWorkerThread extends Thread {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
+
+    private Socket socket;
+
+    public HttpConnectionWorkerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head" +
+                    "<body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                            "Content-Length: " + html.getBytes().length + CRLF + // HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            LOGGER.info(" * Connection Processing Finished...");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+            //TODO Handle later
+        }
+
+    }
+}
