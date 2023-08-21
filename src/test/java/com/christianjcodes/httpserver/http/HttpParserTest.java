5734d851cea8b1077b6d3a3d8c52737991475c29
Christian Johnson <cartian4@gmail.com>
2023-08-21 06:30:11 -0400

    Add TestInstance to HttpParserTest

--- src/test/java/com/christianjcodes/httpserver/http/HttpParserTest.java
+++ src/test/java/com/christianjcodes/httpserver/http/HttpParserTest.java
@@ -2,9 +2,11 @@ package com.christianjcodes.httpserver.http;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HttpParserTest {
 
     private HttpParser httpParser;
