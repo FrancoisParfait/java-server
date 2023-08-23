3ea8334164b7d6b71e63bb2fcb1d3efd8405bf5c
Christian Johnson <cartian4@gmail.com>
2023-08-23 06:30:23 -0400

    Add constructors to HttpParsingException.java

--- src/main/java/com/christianjcodes/httpserver/http/HttpParsingException.java
+++ src/main/java/com/christianjcodes/httpserver/http/HttpParsingException.java
@@ -4,5 +4,14 @@ public class HttpParsingException extends Exception {
 
     private HttpStatusCode errorCode;
 
+    public HttpParsingException(HttpStatusCode errorCode) {
+        super(errorCode.MESSAGE);
+        this.errorCode = errorCode;
+    }
+
+    public HttpStatusCode getErrorCode() {
+        return errorCode;
+    }
+
 
 }
