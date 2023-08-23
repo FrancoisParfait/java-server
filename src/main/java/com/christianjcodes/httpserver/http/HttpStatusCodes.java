51ab8d38d659a362ddbfea5d9374b16903c95146
Christian Johnson <cartian4@gmail.com>
2023-08-23 06:27:39 -0400

    Add Error Message codes to HttpStatusCodes.java

--- src/main/java/com/christianjcodes/httpserver/http/HttpStatusCodes.java
+++ src/main/java/com/christianjcodes/httpserver/http/HttpStatusCodes.java
@@ -1,8 +1,15 @@
 package com.christianjcodes.httpserver.http;
 
 public enum HttpStatusCodes {
+    /* --- CLIENT ERRORS --- */
+    CLIENT_ERROR_400_BAD_REQUEST(400, "Bad Request"),
+    CLIENT_ERROR_401_METHOD_NOT_ALLOWED(401, "Method Not Allowed"),
+    CLIENT_ERROR_414_URI_TOO_LONG(400, "URI Too Long"),
 
-    CLIENT_ERROR_400_BAD_REQUEST(400, "Bad Request");
+    /* --- SERVER ERRORS --- */
+    SERVER_ERROR_500_INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    SERVER_ERROR_501_NOT_IMPLEMENTED(501, "Not Implemented"),
+    ;
 
     public final int STATUS_CODE;
     public final String MESSAGE;
