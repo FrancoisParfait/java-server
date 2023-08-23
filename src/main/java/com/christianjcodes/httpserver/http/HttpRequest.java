9f73d763e06640e2382af6ed95f244e095e5c25b
Christian Johnson <cartian4@gmail.com>
2023-08-23 06:10:36 -0400

    Add HttpRequest method to HttpRequest.java, create enum HttpMethod

--- src/main/java/com/christianjcodes/httpserver/http/HttpRequest.java
+++ src/main/java/com/christianjcodes/httpserver/http/HttpRequest.java
@@ -2,6 +2,12 @@ package com.christianjcodes.httpserver.http;
 
 public class HttpRequest extends HttpMessage {
 
+    private String method;
+    private String requestTarget;
+    private String httpVersion;
 
+    HttpRequest() {
+
+    }
 
 }
