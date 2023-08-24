4fc37ac0583ceff56a91640e8502e98b55115bb5
Christian Johnson <cartian4@gmail.com>
2023-08-24 05:45:23 -0400

    Add processingDataBuffer StringBuilder

--- src/main/java/com/christianjcodes/httpserver/http/HttpParser.java
+++ src/main/java/com/christianjcodes/httpserver/http/HttpParser.java
@@ -21,7 +21,11 @@ public class HttpParser {
 
         HttpRequest request = new HttpRequest();
 
-        parseRequestLine(reader, request);
+        try {
+            parseRequestLine(reader, request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         parseHeaders(reader, request);
         parseBody(reader, request);
 
@@ -30,13 +34,24 @@ public class HttpParser {
 
     private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException {
 
+        StringBuilder processingDataBuffer = new StringBuilder();
+
         int _byte;
         while ((_byte = reader.read()) >=0) {
             if (_byte == CR) {
                 _byte = reader.read();
                 if (_byte == LF) {
+
+                    LOGGER.debug("Request Line to Process : {}", processingDataBuffer.toString());
+
                     return;
                 }
+            }
+
+            if (_byte == SP) {
+                // TODO Process previous data
+            } else {
+                processingDataBuffer.append((char)_byte);
             }
         }
 
