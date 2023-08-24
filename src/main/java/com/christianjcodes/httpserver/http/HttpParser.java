9eb8f3adedd6e897af12b3333bc1a70eb554c049
Christian Johnson <cartian4@gmail.com>
2023-08-24 05:38:36 -0400

    Add while loop to check for CRLF in HttpParser

--- src/main/java/com/christianjcodes/httpserver/http/HttpParser.java
+++ src/main/java/com/christianjcodes/httpserver/http/HttpParser.java
@@ -3,6 +3,7 @@ package com.christianjcodes.httpserver.http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -27,7 +28,17 @@ public class HttpParser {
         return request;
     }
 
-    private void parseRequestLine(InputStreamReader reader, HttpRequest request) {
+    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException {
+
+        int _byte;
+        while ((_byte = reader.read()) >=0) {
+            if (_byte == CR) {
+                _byte = reader.read();
+                if (_byte == LF) {
+                    return;
+                }
+            }
+        }
 
     }
 
