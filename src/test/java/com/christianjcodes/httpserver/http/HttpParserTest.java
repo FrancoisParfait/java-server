4564e236fa54e9ce1801ca7b4c2c77a4d09343d2
Christian Johnson <cartian4@gmail.com>
2023-08-30 06:14:25 -0400

    Add parseHttpEmptyRequestLine test, fix CRLF in the Htttp test InputStreams from \n to \r\n

--- src/test/java/com/christianjcodes/httpserver/http/HttpParserTest.java
+++ src/test/java/com/christianjcodes/httpserver/http/HttpParserTest.java
@@ -33,11 +33,6 @@ class HttpParserTest {
         }
 
         assertEquals(request.getMethod(), HttpMethod.GET);
-
-        // Need to go through all the files in the gitHub and check if anything is wrong.
-        // So far went through pretty much everything and I don't see any errors. Maybe I'll
-        // just keep going and see if it resolves itself.
-
     }
 
     @Test
@@ -79,23 +74,36 @@ class HttpParserTest {
         }
     }
 
+    @Test
+    void parseHttpEmptyRequestLine() {
+
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateBadTestCaseEmptyRequestLine()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+    }
+
     private InputStream generateValidGETTestCase() {
-        String rawData = "GET / HTTP/1.1\n" +
-                "Host: localhost:8080\n" +
-                "Connection: keep-alive\n" +
-                "Cache-Control: max-age=0\n" +
-                "sec-ch-ua: \"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"\n" +
-                "sec-ch-ua-mobile: ?0\n" +
-                "sec-ch-ua-platform: \"Linux\"\n" +
-                "Upgrade-Insecure-Requests: 1\n" +
-                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36\n" +
-                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\n" +
-                "Sec-Fetch-Site: none\n" +
-                "Sec-Fetch-Mode: navigate\n" +
-                "Sec-Fetch-User: ?1\n" +
-                "Sec-Fetch-Dest: document\n" +
-                "Accept-Encoding: gzip, deflate, br\n" +
-                "Accept-Language: en-US,en;q=0.9\n" +
+        String rawData = "GET / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "sec-ch-ua: \"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Linux\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
+                "Accept-Language: en-US,en;q=0.9\r\n" +
                 "\n";
 
         InputStream inputStream = new ByteArrayInputStream(
@@ -108,10 +116,10 @@ class HttpParserTest {
     }
 
     private InputStream generateBadTestCaseMethodName1() {
-        String rawData = "GeT / HTTP/1.1\n" +
-                "Host: localhost:8080\n" +
-                "Accept-Language: en-US,en;q=0.9\n" +
-                "\n";
+        String rawData = "GeT / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en;q=0.9\r\n" +
+                "\r\n";
 
         InputStream inputStream = new ByteArrayInputStream(
                 rawData.getBytes(
@@ -123,10 +131,10 @@ class HttpParserTest {
     }
 
     private InputStream generateBadTestCaseMethodName2() {
-        String rawData = "GETTTT / HTTP/1.1\n" +
-                "Host: localhost:8080\n" +
-                "Accept-Language: en-US,en;q=0.9\n" +
-                "\n";
+        String rawData = "GETTTT / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en;q=0.9\r\n" +
+                "\r\n";
 
         InputStream inputStream = new ByteArrayInputStream(
                 rawData.getBytes(
@@ -138,10 +146,25 @@ class HttpParserTest {
     }
 
     private InputStream generateBadTestCaseRequestLineInvNumItems1() {
-        String rawData = "GET / AAAAAA HTTP/1.1\n" +
-                "Host: localhost:8080\n" +
-                "Accept-Language: en-US,en;q=0.9\n" +
-                "\n";
+        String rawData = "GET / AAAAAA HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en;q=0.9\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
+    }
+
+    private InputStream generateBadTestCaseEmptyRequestLine() {
+        String rawData = "\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en;q=0.9\r\n" +
+                "\r\n";
 
         InputStream inputStream = new ByteArrayInputStream(
                 rawData.getBytes(
