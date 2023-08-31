c0ee67bcdc459a0cf2a4895dbcf529d7791da295
Christian Johnson <cartian4@gmail.com>
2023-08-31 06:14:25 -0400

    Add setter for originalHttpVersion, add Error 505 to HttpStatusCode

--- src/main/java/com/christianjcodes/httpserver/http/HttpRequest.java
+++ src/main/java/com/christianjcodes/httpserver/http/HttpRequest.java
@@ -38,4 +38,14 @@ public class HttpRequest extends HttpMessage {
         }
         this.requestTarget = requestTarget;
     }
+
+    public void setHttpVersion(String originalHttpVersion) throws BadHttpVersionException, HttpParsingException {
+        this.originalHttpVersion = originalHttpVersion;
+        this.bestCompatibleHttpVersion = HttpVersion.getBestCompatibleVersion(originalHttpVersion);
+        if(this.bestCompatibleHttpVersion == null) {
+            throw new HttpParsingException(
+                    HttpStatusCode.SERVER_ERROR_505_HTTP_VERSION_NOT_SUPPORTED
+            );
+        }
+    }
 }
