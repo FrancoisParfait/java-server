39d2376931aa234014df16b7646d0f4406d216f3
Christian Johnson <cartian4@gmail.com>
2023-08-31 05:30:24 -0400

    Create HttpVersion, add objects and constructor to it

---
+++ src/main/java/com/christianjcodes/httpserver/http/HttpVersion.java
@@ -0,0 +1,15 @@
+package com.christianjcodes.httpserver.http;
+
+public enum HttpVersion {
+    HTTP_1_1;
+
+    public final String LITERAL;
+    public final int MAJOR;
+    public final int MINOR;
+
+    HttpVersion(String LITERAL, int MAJOR, int MINOR) {
+        this.LITERAL = LITERAL;
+        this.MAJOR = MAJOR;
+        this.MINOR = MINOR;
+    }
+}
